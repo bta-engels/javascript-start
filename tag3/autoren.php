e2a890ffcabae122df29d4ceb8d1a64e5385d87a
berndengels <engels@f50.de>
2021-04-21 16:23:24 +0200

    stuff

--- tag3/autoren.php
+++ tag3/autoren.php
@@ -7,8 +7,27 @@
 </div>
 
 <script>
+    var divAuthors = document.querySelector(".col:first-child"),
+	    divAuthor = document.querySelector(".col:last-child"),
+        ul = document.createElement("ul"),
+        li, item;
     // ajax request per GET methode
     const URL = "http://bta-movies-start.loc";
+    $.get(URL + "/api/authors", function(response) {
+    	// antwort verarbeiten
+        for(item of response) {
+        	li = document.createElement("li");
+        	li.id = item.id;
+        	li.innerText = item.firstname + " " + item.lastname;
+        	ul.append(li);
+        }
+	    divAuthors.append(ul);
+        // todo: click on author => get author data per ajax request (/api/author/ID)
+    });
+
+    function getAuthor(id) {
+    }
+
     // API liefert alle autoren per JSON, baue damit liste (li) zusammen
     // loop über authors array
     // li - elem bauen
@@ -18,6 +37,13 @@
 <style>
     .col {
         border: 1px solid #999;
+    }
+	.movies {
+		padding-left: 1.0rem;
+	}
+    .movies li {
+        font-size: 0.6rem;
+        margin-left: 0;
     }
 </style>
 <?php require '../inc/footer.html'; ?>
