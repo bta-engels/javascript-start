8b334ad6d33db2e2213b749613656505f8687ebf
berndengels <engels@f50.de>
2022-02-02 09:28:34 +0100

    vor- und nachname

---
+++ tag1/name.php
@@ -0,0 +1,21 @@
+<?php require '../inc/header.html'; ?>
+
+<h1>Bitte Vor- und Nachname eintragen<!-- hier Vor- und Nachname ausgeben --></h1>
+
+<form class="mt-5">
+    <div class="form-group">
+        <label for="a">Zahl A</label>
+        <input type="text" id="firstname" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="b">Zahl B</label>
+        <input type="text" id="lastname" class="form-control">
+    </div>
+    <div class="form-group">
+        <input type="button" id="submit" value="rechne" class="btn btn-primary">
+    </div>
+</form>
+<script>
+
+</script>
+<?php require '../inc/footer.html'; ?>
