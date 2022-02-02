2ca68cb3f28e17bbdfb0073e403f47e293edc628
bakrihamza <bakrihamza2209@gmail.com>
2022-02-02 12:49:42 +0100

    stuff

--- tag1/sync-text.php
+++ tag1/sync-text.php
@@ -4,10 +4,18 @@
 
 <form class="mt-5">
     <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" id="name" class="form-control">
+        <label for="text">Name</label>
+        <input type="text" id="text" class="form-control">
     </div>
 </form>
 <script>
+    var h1 = document.querySelector('h1'),
+        inpText = document.getElementById('text');
+
+    inpText.onkeyup = (e) => {
+        //console.info(e.target.value)
+        h1.innerText = e.target.value
+    }
 </script>
+
 <?php require '../inc/footer.html'; ?>
