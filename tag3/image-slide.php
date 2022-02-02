9152fd84aa4fba3cd581fdad86c66ccc73641893
berndengels <engels@f50.de>
2022-02-02 13:32:46 +0100

    stuff

--- tag3/image-slide.php
+++ tag3/image-slide.php
@@ -1,20 +1,22 @@
 <?php require '../inc/header.html'; ?>
 
-<div class="row">
-    <img id="image" src="../img/fahrrad.jpg" class="align-self-center m-auto" height="500" alt="Slider" title="Slider">
+<div class="w-100 row bg-dark">
+    <img id="image" src="../img/fahrrad.jpg" class="align-self-center m-auto" height="400" alt="Slider" title="Slider">
 </div>
+
 <script>
-	const imageList = [
-		'cowboy.jpg',
-		'ziel.jpg',
-		'vw.jpg',
-		'haus_in_strasse.jpg',
-		'hooge_parking.jpg',
-		'nebel_morgen.jpg',
-		'venus.jpg',
-		'femö.jpg',
-		'nebel-steg.jpg'
-	];
+const delay = 3000,
+    imageList = [
+    'cowboy.jpg',
+    'ziel.jpg',
+    'vw.jpg',
+    'haus_in_strasse.jpg',
+    'hooge_parking.jpg',
+    'nebel_morgen.jpg',
+    'nebel_steg.jpg',
+    'venus.jpg',
+    'femö1.jpg'
+];
 
 </script>
 <?php require '../inc/footer.html'; ?>
