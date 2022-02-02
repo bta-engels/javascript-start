596fb5a48bf595db9a05209e7d557f93ad691696
berndengels <engels@f50.de>
2022-02-02 13:12:33 +0100

    stuff

--- tag3/image-slide-infinite.php
+++ tag3/image-slide-infinite.php
@@ -4,6 +4,17 @@
     <img id="image" src="../img/fahrrad.jpg" class="align-self-center m-auto" height="500" alt="Slider" title="Slider">
 </div>
 <script>
+    const imageList = [
+		'cowboy.jpg',
+	    'ziel.jpg',
+	    'vw.jpg',
+        'haus_in_strasse.jpg',
+        'hooge_parking.jpg',
+        'nebel_morgen.jpg',
+        'venus.jpg',
+        'femö.jpg',
+        'nebel-steg.jpg'
+    ];
 </script>
 <?php require '../inc/footer.html'; ?>
 
