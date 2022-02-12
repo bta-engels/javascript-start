2cca83b5882f71b4111811f1916ba95f5ba18a0a
berndengels <engels@f50.de>
2022-02-12 11:35:56 +0100

    stuff

--- aufgaben/buch.php
+++ aufgaben/buch.php
@@ -5,21 +5,39 @@
 <script>
 var divBook = document.getElementById('book'),
     table = document.createElement('table'),
-	buch = {
+	book = {
 		"id": 1,
 		"autor": "Umberto Eco",
 		"title": "Der Name der Rose",
-		"Jahr": 1980,
+		"jahr": 1980,
 		"preis": 20.00
-	};
+	},
+//    bookCopyByValue is copy of book
+    bookCopyByValue = Object.assign({}, book),
+    // or other method
+//    bookCopyByValue = JSON.parse(JSON.stringify(book))
+//    bookReferenceByValue is reference of book
+    bookReferenceByValue = book,
+    a = 1,
+    b = a
+    ;
+b = 2
+bookReferenceByValue.title = "Bin jetzt eine Referenz"
+bookCopyByValue.title = "Und jetzt eine richtige  Kopie"
 
-for(key in buch) {
+console.info("book", book)
+console.info("bookCopyByValue", bookCopyByValue)
+//console.info("book", book)
+console.info("bookReferenceByValue", bookReferenceByValue)
+console.info("a", a, "b", b)
+
+for(key in book) {
 	let tr = document.createElement('tr'),
 		th = document.createElement('th'),
 		td = document.createElement('td');
 
 	th.innerText = key.toUpperCase();
-    td.innerText = buch[key];
+    td.innerText = book[key];
     tr.appendChild(th);
 	tr.appendChild(td);
     table.appendChild(tr);
