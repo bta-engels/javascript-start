2456cae3403d4739b9aeb45a3da63d02993a0fa9
memoray <102960499+memoray@users.noreply.github.com>
2022-04-13 14:31:44 +0200

    image slide

--- tag3/image-slide.php
+++ tag3/image-slide.php
@@ -1,22 +1,47 @@
 <?php require '../inc/header.html'; ?>
 
 <div class="w-100 row bg-dark">
-    <img id="image" src="../img/fahrrad.jpg" class="align-self-center m-auto" height="400" alt="Slider" title="Slider">
+    <img id="image" src="" class="align-self-center m-auto" height="400" alt="Slider" title="Slider">
 </div>
 
 <script>
-const delay = 3000,
+const delay = 1000,
+    loop = false,
     imageList = [
-    'cowboy.jpg',
-    'ziel.jpg',
-    'vw.jpg',
-    'haus_in_strasse.jpg',
-    'hooge_parking.jpg',
-    'nebel_morgen.jpg',
-    'nebel_steg.jpg',
-    'venus.jpg',
-    'femö1.jpg'
-];
+        'cowboy.jpg',
+        'ziel.jpg',
+        'vw.jpg',
+        'haus_in_strasse.jpg',
+        'hooge_parking.jpg',
+        'nebel_morgen.jpg',
+        'nebel_steg.jpg',
+        'venus.jpg',
+        'femö1.jpg',
+        'see.jpg'
+    ],
+    img = document.getElementById('image'),
+    countImages = imageList.length;
+
+let counter = 0
+let interval = setInterval(function() {
+    console.info(counter)
+    img.src = '../img/' + imageList[counter];
+    counter++
+    if (counter >= countImages){
+        if (loop === false){
+            //stop interval
+            clearInterval(interval)
+        }   else {
+            counter = 0
+        }
+    }
+}, delay)
+
+//stop interval
+//clearInterval(interval)
+
+//img.src = '../img/' + imageList[8];
+
 </script>
 <?php require '../inc/footer.html'; ?>
 
